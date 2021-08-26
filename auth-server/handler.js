e1a6d19d81da83a091cc2c41ff7f9f7839cc4479
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-08-26 20:31:03 +0100

    Corrected position of environment variables in serverless file, edited redirect URI in handler file.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,42 @@
-'use strict';
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+// scope is that selected when we registered the app with Google in the API console
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+/* credentials are those provided by Google when we registered the app, referenced here as the process
+environment variables that we entered in the serverless file, which in turn reference the config file */
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: "https://Penny167.github.io/meet/",
+  javascript_origins: ["https://Penny167.github.io", "http://localhost:3000"],
+};
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 
-module.exports.hello = async (event) => {
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris
+);
+
+// This function retrieves a URL with an authorization code when a user authorizes the app via Google
+module.exports.getAuthURL = async () => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
