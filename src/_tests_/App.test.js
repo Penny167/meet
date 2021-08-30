e5ba187eb86c0ea4a0be6b100a4955001e77bb10
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-08-30 16:42:12 +0100

    Added new event list component and updated app to render it and test to check it is being rendered.

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
+import EventList from '../EventList';
 
 // The scope of these tests is the App component
 describe('<App /> component', () => {
