79fd1bf577ab7983aab08a9476ab23d4e29abb86
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-08-31 14:34:45 +0100

    Added class name to CitySearch container and refactored app test file to create appwrapper once before all tests on the app cpmponent.

--- src/Event.js
+++ src/Event.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Event extends Component {
   render () {
     return (
-      <div className="event"></div>
+      <div className="Event"></div>
     );
   }
 }
--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -6,14 +6,17 @@ import CitySearch from '../CitySearch';
 
 // The scope of these tests is the App component
 describe('<App /> component', () => {
+  let AppWrapper;
+  
+  beforeAll(() => {  
+    AppWrapper = shallow(<App />)
+  });
 
   test('should render the EventList', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
 
   test('should render the CitySearch component', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
