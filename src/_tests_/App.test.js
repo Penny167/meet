9c0cad5a8ad06d0b97032478063ec57c05f6d232
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-08-31 14:09:34 +0100

    Added CitySearch component and imported in to App and App.test.

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 // The scope of these tests is the App component
 describe('<App /> component', () => {
@@ -9,6 +10,11 @@ describe('<App /> component', () => {
   test('should render the EventList', () => {
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('should render the CitySearch component', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
 });
