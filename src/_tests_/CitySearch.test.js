97feabc1744676f543d1f643b0e6eb8493f680d8
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-09-02 13:42:35 +0100

    Refactored CitySearch test file to set CitySearchWraper before all tests.

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -5,25 +5,26 @@ import { extractLocations } from '../api';
 import mockData from '../mock-data';
 
 describe('<CitySearch /> component', () => {
+  let CitySearchWrapper;
   
+  beforeAll(() => {  
+    CitySearchWrapper = shallow(<CitySearch />)
+  });
+
   test('should render text input with class name city', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
 
   test('should render list of city suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
 
   test('value of text input should equal query state of CitySearch component', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
 
   test('query state should update dynamically when user types into text input', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     CitySearchWrapper.setState({ query: 'Munich' });
     const eventObject = { target: { value: 'Berlin' }};
     CitySearchWrapper.find('.city').simulate('change', eventObject); // Simulates input value updating to Berlin when user types
@@ -31,7 +32,6 @@ describe('<CitySearch /> component', () => {
   });
 
   test('list of suggestions rendered should match list in component state', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const locations = extractLocations(mockData);
     CitySearchWrapper.setState({ suggestions: locations }); // set the suggestions state with mock data
     const suggestions = CitySearchWrapper.state('suggestions');
