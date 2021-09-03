6a306f287ef034124075a78f172638ad056459f0
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-09-03 13:11:46 +0100

    Created test file for Event component with basic structure.

---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import mockData from '../mock-data';
+
+describe('<Event /> component', () => {
+  let EventWrapper;
+  
+  beforeAll(() => { 
+    EventWrapper = shallow(<Event />)
+  });
+
+})
