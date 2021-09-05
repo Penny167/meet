72159707a0af03206635cad1e1110bcabf10fb38
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-09-05 12:12:54 +0100

    Remaining tests added and Event file updated so toggleDetails function correctly updates state when clicked and toggles event details.

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    buttonText: 'Show details',
+    show: false
+  }
+
+  toggleDetails = () => {
+    if (this.state.buttonText === 'Show details'){
+      this.setState({ 
+        buttonText: 'Hide details',
+        show: true
+      })
+      } else {
+      this.setState({ 
+      buttonText: 'Show details',
+      show: false
+      })
+    }
+  }
+
   render () {
+    const { event } = this.props;
     return (
-      <div className="Event"></div>
+      <div className="Event">
+        <h1 className="title">{event.summary}</h1>
+        <p className="start">{event.start.dateTime}</p>
+        <p className="timezone">({event.start.timeZone})</p>
+        <p className="email">{event.organizer.email}</p>
+        <p className="location">{event.location}</p> 
+        <div className="details" style={{ display: this.state.show ? "block" : "none"}}>
+          <h3 className="subtitle">About event:</h3>
+          <a className="link" href={event.htmlLink}>See details on calendar</a>
+          <p className="description">{event.description}</p>
+        </div> 
+        <button className="button" onClick={this.toggleDetails}>{this.state.buttonText}</button>
+      </div>
+      
     );
   }
 }
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -5,9 +5,129 @@ import mockData from '../mock-data';
 
 describe('<Event /> component', () => {
   let EventWrapper;
+  let event = mockData[0]; // This will simulate the event data passed as props to each Event by the EventList component
   
   beforeAll(() => { 
-    EventWrapper = shallow(<Event />)
+    EventWrapper = shallow(<Event event={event}/>)
   });
 
-})
+  test('each event rendered should have a title', () => {
+    expect(EventWrapper.find('.title')).toHaveLength(1); 
+  });
+
+  test('title should be value of summary field for each event', () => {
+    const title = event.summary;
+    expect(EventWrapper.find('.title').text()).toBe(title); 
+  });
+
+  test('each event rendered should have a start time', () => {
+    expect(EventWrapper.find('.start')).toHaveLength(1); 
+  });
+
+  test('start time should be value of dateTime field for each event', () => {
+    const start = event.start.dateTime;
+    expect(EventWrapper.find('.start').text()).toBe(start); 
+  });
+
+  test('there should be a time zone rendered for each event', () => {
+    expect(EventWrapper.find('.timezone')).toHaveLength(1); 
+  });
+
+  test('time zone should be value of timeZone field for each event', () => {
+    const timeZone = event.start.timeZone;
+    expect(EventWrapper.find('.timezone').text()).toBe('(' + timeZone + ')'); 
+  });
+
+  test('there should be an organizer email rendered for each event', () => {
+    expect(EventWrapper.find('.email')).toHaveLength(1); 
+  });
+
+  test('email should be value of organizer email field for each event', () => {
+    const email = event.organizer.email;
+    expect(EventWrapper.find('.email').text()).toBe(email); 
+  });
+
+  test('there should be a location rendered for each event', () => {
+    expect(EventWrapper.find('.location')).toHaveLength(1); 
+  });
+
+  test('location should be value of location field for each event', () => {
+    const location = event.location;
+    expect(EventWrapper.find('.location').text()).toBe(location); 
+  });
+
+  test('there should be a show details button rendered for each event', () => {
+    expect(EventWrapper.find('.button')).toHaveLength(1); 
+  });
+
+  test('there should be a container with additional event details for each event', () => {
+    expect(EventWrapper.find('.details')).toHaveLength(1);
+  });
+
+  test('there should be a subtitle rendered for each event', () => {
+    expect(EventWrapper.find('.subtitle')).toHaveLength(1); 
+  });
+
+  test('subtitle should say: About event:', () => {
+    const subtitle = 'About event:';
+    expect(EventWrapper.find('.subtitle').text()).toBe(subtitle); 
+  });
+
+  test('there should be a link to the event on the google calendar rendered for each event', () => {
+    expect(EventWrapper.find('.link')).toHaveLength(1); 
+  });
+
+  test('Link should say: See details on calendar:', () => {
+    const linkText = 'See details on calendar';
+    expect(EventWrapper.find('.link').text()).toBe(linkText); 
+  });
+
+  test('Link should link to htmlLink of specified event', () => {
+    const link = event.htmlLink;
+    expect(EventWrapper.find('a').prop('href')).toBe(link); 
+  });
+
+  test('there should be a description rendered for each event', () => {
+    expect(EventWrapper.find('.description')).toHaveLength(1); 
+  });
+
+  test('button text should equal buttonText state', () => {
+    const buttonText = EventWrapper.state('buttonText');
+    expect(EventWrapper.find('.button').text()).toBe(buttonText);
+  });
+
+  test('if button is clicked in default position button text should change to Hide details', () => {
+    EventWrapper.setState({ buttonText: 'Show details'});
+    EventWrapper.find('.button').simulate('click');
+    expect(EventWrapper.state('buttonText')).toBe('Hide details');
+  })
+
+  test('details container style attribute should equal display: none when show state is false', () => {
+    EventWrapper.setState({ show: false});
+    expect(EventWrapper.find('.details').prop('style')).toEqual({display: 'none'});
+  })
+
+  test('details container style attribute should equal display: block when show state is true', () => {
+    EventWrapper.setState({ show: true});
+    expect(EventWrapper.find('.details').prop('style')).toEqual({display: 'block'});
+  })
+
+  test('if button is clicked in default/collapsed position show state should change to true', () => {
+    EventWrapper.setState({ buttonText: 'Show details', show: false });
+    EventWrapper.find('.button').simulate('click');
+    expect(EventWrapper.state('show')).toBe(true);
+  }) 
+
+  test('if button is clicked in expanded position show state should change back to false', () => {
+    EventWrapper.setState({ buttonText: 'Hide details', show: true });
+    EventWrapper.find('.button').simulate('click');
+    expect(EventWrapper.state('show')).toBe(false);
+  })
+
+  test('if button is clicked in expanded position button text should change back to Show details', () => {
+    EventWrapper.setState({ buttonText: 'Hide details'});
+    EventWrapper.find('.button').simulate('click');
+    expect(EventWrapper.state('buttonText')).toBe('Show details');
+  })
+
+});
