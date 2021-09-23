e342ab02f1ae79c7655b9cbdcda3d0178e689a7b
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-09-23 17:35:03 +0100

    Atatus installed for app performance monitoring and imported into index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('10b8f865c9304231862d88babf3e192d').install();
 
 ReactDOM.render(
   <React.StrictMode>
