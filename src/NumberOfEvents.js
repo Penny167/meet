1e2787d38909b0165f83c251ba9c0a37f95e1aaf
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-09-29 18:40:50 +0100

    Added new ErrorAlert alert type and imported in to NumberOfEvents. Updated setEventNumber function to display alert if invalid number selected.

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   
   state = { 
-    eventsNumber: 12
+    eventsNumber: 12,
+    errorText: ''
   }
 
   setEventNumber = (e) => {
-    this.setState({ eventsNumber: e.target.value });
+    if (e.target.value < 1 || e.target.value > 32) {
+      this.setState({ 
+        eventsNumber: e.target.value,
+        errorText: 'Please select a number between 1 and 32'
+      });
+    } else {
+    this.setState({ 
+      eventsNumber: e.target.value,
+      errorText: ''
+    });
     this.props.updateEvents(null, e.target.value);
-  } 
+    } 
+  }
 
   render() {
     return(
       <div>
         <p>Number of events to display:</p>
         <input className="numberOfEvents" value={this.state.eventsNumber} onChange={this.setEventNumber}></input>
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
